421ac7a1a4bfbfe9a8b26cb79565ec1e2407780b
ЭФкудфМЭ <arelav90@mail.ru>
2018-05-30 21:11:10 +0300

    simplify

--- password_strength.py
+++ password_strength.py
@@ -13,41 +13,19 @@ def get_password_strength(test_password, black_list_path):
     security_point = 1
     min_length_of_password = 6
     if len(test_password) > min_length_of_password:
-        print('1) Lenght test - OK')
         security_point += 1
         blacklist = get_passwords_blacklist(black_list_path)
-        if test_password in blacklist:
-            print('2) Your pass in blacklist - FAILED')
-        else:
-            print('2) Blacklist test - OK')
+        if not(test_password in blacklist):
             security_point += 1
-
-        if test_password.islower() or test_password.isupper():
-            print('3) Case sensitivity test - FAILED')
-        else:
-            print('3) Case sensitivity test - OK')
+        if not(test_password.islower()) or not(test_password.isupper()):
             security_point += 2
-
         if re.findall('\d+', test_password):
-            print('4) Include number test - OK')
             security_point += 1
-        else:
-            print('4) Include number test - FAILED')
-
         if re.findall('\W+', test_password):
-            print('5) Include special symbols test - OK')
             security_point += 2
-        else:
-            print('5) Include special symbols test - FAILED')
-
-        if re.match(r'\d{2}-\d{2}-\d{4}', test_password) or re.match(
-                r'(7|8|\+7)\(\d{3}\)\d{3}-\d{2}-\d{2}', test_password):
-            print('6) Include date or phone test - FAILED')
-        else:
-            print('6) Include date or phone test - OK')
+        if not(re.match(r'\d{2}-\d{2}-\d{4}', test_password)) or not(re.match(
+                r'(7|8|\+7)\(\d{3}\)\d{3}-\d{2}-\d{2}', test_password)):
             security_point += 2
-    else:
-        print('1) Lenght test - FAILED')
     return security_point
 
 
