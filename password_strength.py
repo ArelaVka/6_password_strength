33714a244ea440632a0c7a18f01217e74c4e62b4
ЭФкудфМЭ <arelav90@mail.ru>
2018-05-31 00:48:24 +0300

    another simplify

--- password_strength.py
+++ password_strength.py
@@ -12,20 +12,20 @@ def get_passwords_blacklist(path_to_file):
 def get_password_strength(test_password, black_list_path):
     security_point = 1
     min_length_of_password = 6
+    blacklist = get_passwords_blacklist(black_list_path)
     if len(test_password) > min_length_of_password:
         security_point += 1
-        blacklist = get_passwords_blacklist(black_list_path)
-        if not(test_password in blacklist):
-            security_point += 1
-        if not(test_password.islower()) or not(test_password.isupper()):
-            security_point += 2
-        if re.findall('\d+', test_password):
-            security_point += 1
-        if re.findall('\W+', test_password):
-            security_point += 2
-        if not(re.match(r'\d{2}-\d{2}-\d{4}', test_password)) or not(re.match(
-                r'(7|8|\+7)\(\d{3}\)\d{3}-\d{2}-\d{2}', test_password)):
-            security_point += 2
+    if not (test_password in blacklist):
+        security_point += 1
+    if not (test_password.islower()) or not (test_password.isupper()):
+        security_point += 2
+    if re.findall('\d+', test_password):
+        security_point += 1
+    if re.findall('\W+', test_password):
+        security_point += 2
+    if not (re.match(r'\d{2}-\d{2}-\d{4}', test_password)) or not (re.match(
+            r'(7|8|\+7)\(\d{3}\)\d{3}-\d{2}-\d{2}', test_password)):
+        security_point += 2
     return security_point
 
 
