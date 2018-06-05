350fe47c79f10f1b59ee581a6fd54ea1df4df61b
maltsev <arelav90@mail.ru>
2018-06-05 11:12:57 +0300

    delete custom sum func

--- password_strength.py
+++ password_strength.py
@@ -12,11 +12,8 @@ def load_blacklist(path_of_file):
         return None
 
 
-def is_in_blacklist(test_password, input_blacklist):
-    if len(input_blacklist) > 1:
-        return test_password not in input_blacklist
-    else:
-        return False
+def is_in_blacklist(test_password, blacklist):
+    return test_password in blacklist if blacklist else False
 
 
 def check_length(test_password):
@@ -39,29 +36,33 @@ def has_spec_symbol(test_password):
 def has_no_date_and_phone(test_password):
     return (not (re.search('\d{2}-\d{2}-\d{4}', test_password)) and
             not (re.search('\d{2}\.\d{2}\.\d{4}', test_password)) and
-            not (re.search('(7|8|\+7)\(\d{3}\)\d{3}-\d{2}-\d{2}', test_password)))
-
-
-def sum_all_factors(*args):
-    return sum(args)
+            not (re.search(
+                '(7|8|\+7)\(\d{3}\)\d{3}-\d{2}-\d{2}', test_password))
+            )
 
 
 if __name__ == '__main__':
     password_strength = 1
     if len(sys.argv) > 1 and os.path.exists(sys.argv[1]):
         blacklist = load_blacklist(sys.argv[1])
-        if len(blacklist) > 1:
+        if blacklist:
             print('Blacklist loading - OK')
     else:
         blacklist = []
-        print('Blacklist loading - ERROR (File is empty or path is not correct)\n'
+        print('Blacklist loading - ERROR ('
+              'File is empty or path is not correct)\n'
               'Blacklist check is not use.')
 
     password = getpass.getpass(prompt='Enter password to check: ')
     if check_length(password):
-        password_strength = sum_all_factors(
-            password_strength, check_length(password), 2*has_upper_and_lower_case(password), has_number(password),
-            2*has_spec_symbol(password), is_in_blacklist(password, blacklist), 2*has_no_date_and_phone(password)
+        password_strength = sum([
+            password_strength,
+            check_length(password),
+            2*has_upper_and_lower_case(password),
+            has_number(password),
+            2*has_spec_symbol(password),
+            not(is_in_blacklist(password, blacklist)),
+            2*has_no_date_and_phone(password)]
         )
         print('Your password strength is',  password_strength, '(max - 10)')
     else:
